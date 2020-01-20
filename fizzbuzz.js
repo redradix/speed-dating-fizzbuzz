9dc1c1cece17b36d483e5e58b8bf8459bfb8d5e3
Victor Miguez <victor@redradix.com>
2020-01-20 10:49:43 +0100

    lucia herrador

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,10 +4,16 @@
 */
 
 function fizzbuzz(n) {
-  console.log('implement here', n)
+  let array = []
+  for(let i = 0; i < n; i++) {
+    console.log(i)
+    array.push(i)
+  }
+  console.log(array)
 }
 
 fizzbuzz(0)
 fizzbuzz(3)
 fizzbuzz(5)
 fizzbuzz(15)
+
