8cbd2f65ccd8ec07d242c9166ca301fd233a8656
Victor Miguez <victor@redradix.com>
2020-01-20 11:14:52 +0100

    reset

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,14 +4,6 @@
 */
 
 function fizzbuzz(n) {
-  const array = []
-for (let i = 0; i<n; i++){
-  array.push(i);
-  if (i%3===0){
-    i='Fizz';
-  }
-  }
-  return array;
 }
 
 console.log(fizzbuzz(0))
